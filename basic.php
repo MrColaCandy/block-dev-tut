43aa386c5a06520ed3ab18d007ecb97c61dca474
Mahmood <mahmoodkhodadady10@gmail.com>
2024-03-17 09:24:57 +0330

    restore project

--- basic.php
+++ basic.php
@@ -13,9 +13,44 @@
  * @package           create-block
  */
 
-
-
 function basic_block_init() {
-	register_block_type( plugin_dir_path(__FILE__). 'blocks/basic/build/block.json' );
+    // Register block
+    register_block_type( plugin_dir_path(__FILE__). 'blocks/basic/build/block.json' );
+
+    // Add admin menu
+    add_action('admin_menu', 'basic_plugin_menu');
 }
 add_action( 'init', 'basic_block_init' );
+
+function basic_plugin_menu() {
+    // Add a top-level menu page
+    add_menu_page(
+        'Basic',
+        'Basic',
+        'manage_options',
+        'basic-menu',
+        'render_basic_menu',
+        'dashicons-admin-plugins',
+        6
+    );
+}
+
+function render_basic_menu() {
+    // Display the page content
+    ?> <h3>Hello from render basic menu function</h3>
+	<?php
+}
+
+// Enqueue front-end scripts and styles
+function basic_enqueue_frontend_assets() {
+    wp_enqueue_style( 'basic-frontend-css', plugin_dir_url( __FILE__ ) . 'front/css/style.css', array(), time() );
+    wp_enqueue_script( 'basic-frontend-js', plugin_dir_url( __FILE__ ) . 'front/js/main.js', array(), time(), true );
+}
+add_action( 'wp_enqueue_scripts', 'basic_enqueue_frontend_assets' );
+
+// Enqueue admin scripts and styles
+function basic_enqueue_admin_assets() {
+    wp_enqueue_style( 'basic-admin-css', plugin_dir_url( __FILE__ ) . 'back/css/style.css', array(), time() );
+    wp_enqueue_script( 'basic-admin-js', plugin_dir_url( __FILE__ ) . 'back/js/main.js', array(), time(), true );
+}
+add_action( 'admin_enqueue_scripts', 'basic_enqueue_admin_assets' );
